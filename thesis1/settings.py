03ba5e54106a1235bea87af673302f6375b05bad
stellatompazi <51436831+stellatompazi@users.noreply.github.com>
2019-07-01 15:30:06 +0300

    Update settings.py

--- thesis1/settings.py
+++ thesis1/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 #    os.path.join(BASE_DIR, "static")
 #]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/mr/static/'
 
 LOGIN_URL = '/mr/login/'
 
